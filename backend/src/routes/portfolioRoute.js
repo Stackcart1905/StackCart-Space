6938fc81774fc5adde2e85ba8f662d7122cb5f1e
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-08-21 23:35:06 +0530

    type correctd

--- backend/src/routes/portfolioRoute.js
+++ backend/src/routes/portfolioRoute.js
@@ -1,22 +1,39 @@
-import express from "express"; 
-import { getAllPortfolios, createPortfolio, updatePortfolio, deletePortfolio } from "../controllers/portfolio.controllers.js";
-import protectRoute from "../middleware/authMiddleware.js";
+import express from "express";
+import {
+  getAllPortfolios,
+  createPortfolio,
+  updatePortfolio,
+  deletePortfolio,
+} from "../controllers/portfolio.controllers.js";
+import protectRoute from "../middleware/authmiddleware.js";
 import uploadMiddleware from "../middleware/multerMiddleware.js"; // Assume this is your multer instance
 
-const router = express.Router(); 
+const router = express.Router();
 
-router.get("/", getAllPortfolios); 
+router.get("/", getAllPortfolios);
 
-router.post("/", protectRoute, uploadMiddleware.fields([ // Use .fields for multiple data of req.file
-  { name: 'beforeImage', maxCount: 1 },
-  { name: 'afterImage', maxCount: 1 }
-]), createPortfolio); 
+router.post(
+  "/",
+  protectRoute,
+  uploadMiddleware.fields([
+    // Use .fields for multiple data of req.file
+    { name: "beforeImage", maxCount: 1 },
+    { name: "afterImage", maxCount: 1 },
+  ]),
+  createPortfolio
+);
 
-router.patch("/:id", protectRoute, uploadMiddleware.fields([ //  Use .fields for updates multi data req.file
-  { name: 'beforeImage', maxCount: 1 },
-  { name: 'afterImage', maxCount: 1 }
-]), updatePortfolio); 
+router.patch(
+  "/:id",
+  protectRoute,
+  uploadMiddleware.fields([
+    //  Use .fields for updates multi data req.file
+    { name: "beforeImage", maxCount: 1 },
+    { name: "afterImage", maxCount: 1 },
+  ]),
+  updatePortfolio
+);
 
-router.delete("/:id", protectRoute, deletePortfolio); 
+router.delete("/:id", protectRoute, deletePortfolio);
 
 export default router;
--- backend/src/routes/serviceRoute.js
+++ backend/src/routes/serviceRoute.js
@@ -1,13 +1,17 @@
-import express from 'express';
-import { getAllServices, createService, updateService, deleteService } from '../controllers/service.controllers.js';
-import protectRoute from '../middleware/authMiddleware.js'
+import express from "express";
+import {
+  getAllServices,
+  createService,
+  updateService,
+  deleteService,
+} from "../controllers/service.controllers.js";
+import protectRoute from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
- router.get("/" , getAllServices) ; 
- router.post("/" ,protectRoute,  createService) ; 
- router.put("/:id" , protectRoute , updateService) ; 
- router.delete("/:id" , protectRoute , deleteService) ; 
- 
- 
-  export default router;
+router.get("/", getAllServices);
+router.post("/", protectRoute, createService);
+router.put("/:id", protectRoute, updateService);
+router.delete("/:id", protectRoute, deleteService);
+
+export default router;
